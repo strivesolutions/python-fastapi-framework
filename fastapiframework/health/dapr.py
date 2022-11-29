e6176d059e95380fc191d100ec259c9f02149c1e
Michael Dryden <mjdryden@gmail.com>
2022-11-29 13:32:19 -0600

    Fix Dapr check

--- fastapiframework/health/dapr.py
+++ fastapiframework/health/dapr.py
@@ -7,14 +7,17 @@ from fastapiframework.health.health_checker import HealthChecker
 def create_dapr_health_check(endpoint: str) -> HealthChecker:
     def run(name: str) -> HealthCheckResult:
         url = f"{endpoint}/v1.0/healthz"
-        resp = httpx.get(url)
+        try:
+            resp = httpx.get(url)
 
-        return (
-            HealthCheckResult.ok(name)
-            if resp.status_code == 204
-            else HealthCheckResult.unhealthy(
-                name, f"Response from Dapr was {resp.status_code}"
+            return (
+                HealthCheckResult.ok(name)
+                if resp.status_code == 204
+                else HealthCheckResult.unhealthy(
+                    name, f"Response from Dapr was {resp.status_code}"
+                )
             )
-        )
+        except Exception as e:
+            return HealthCheckResult.unhealthy(name, str(e))
 
     return HealthChecker(name="dapr", run=run)
