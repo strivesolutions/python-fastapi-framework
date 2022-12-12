4873bd1b7b11ba29d62a99bf3514bd713efafeb1
Michael Dryden <mjdryden@gmail.com>
2022-12-12 12:11:12 -0600

    Data request events

--- fastapiframework/pubsub/data_request_payload.py
+++ fastapiframework/pubsub/data_request_payload.py
@@ -16,23 +16,6 @@ class DataRequestPayload(CamelCaseModel):
     correlation_id: str
     data: dict
 
-    @staticmethod
-    def from_event_data(source: dict) -> DataRequestPayload:
-        e = CloudEvent(**source)
-        correlation_id = e.data["correlationId"]
-        data = base64.b64decode(e.data["data"])
-        data = json.loads(data)
-
-        return DataRequestPayload(
-            correlation_id=correlation_id,
-            data=data,
-        )
-
-    def unwrap_as(self, cls: Type[T]) -> T:
-        assert issubclass(cls, BaseModel), "cls must be of type BaseModel"
-
-        return cls.parse_obj(self.data)
-
 
 class DataRequestEventData(CamelCaseModel):
     correlation_id: str
